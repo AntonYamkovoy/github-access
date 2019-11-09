a500213c9408f8dd5964c4ad336bbdc83b0b209a
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-11-09 11:12:01 +0000

    testing pygithub

---
+++ github_access/test.py
@@ -0,0 +1,28 @@
+from github import Github
+
+
+
+g = Github("4a78d9e6a5a0602050ccf60acf08cda73c530e96")
+#  4a78d9e6a5a0602050ccf60acf08cda73c530e96 github key
+user = g.get_user("kamilprz")
+repos = user.get_repos()
+
+for repo in repos:
+	print(">",repo.name)
+	collaborators = repo.get_contributors()
+	for c in collaborators:
+		print(">>",c.login)
+
+"""
+repo_name = 'bitcoin/bitcoin'
+repo = g.get_repo(repo_name)
+collaborators = repo.get_collaborators()
+
+for collaborator in collaborators:
+    print(collaborator.login)
+	"""
+
+
+
+
+print("public: ",user.public_repos, " private: ",user.total_private_repos)
