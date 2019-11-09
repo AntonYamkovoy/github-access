581da6ac9190960a94f24b011287c8e495b78f19
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-11-09 13:41:15 +0000

    mongo db cluster added

--- test.py
+++ test.py
@@ -1,28 +1,9 @@
 from github import Github
-import pymongo
-from pymongo import MongoClient
-
-
-
-client = pymongo.MongoClient("mongodb+srv://Anton:Frozenfire1999@sweng-cqjlw.mongodb.net/test?retryWrites=true&w=majority")
-db = client.sweng_data
-
-posts = db.posts
-post_data = {
-    'title': 'Python and MongoDB',
-    'content': 'PyMongo is fun, you guys',
-    'author': 'Anton'
-}
-result = posts.insert_one(post_data)
-print('One post: {0}'.format(result.inserted_id))
-
-client.close()
-
 
 
 
 g = Github("4a78d9e6a5a0602050ccf60acf08cda73c530e96")
-#  4a78d9e6a5a0602050ccf60acf08cda73c530e96 github key
+
 user = g.get_user("AntonYamkovoy")
 repos = user.get_repos()
 
