669ee712569d18c8aa26a99b4d81b4c19cb10e8c
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-11-10 19:59:09 +0000

    Update test.py

--- github_access/test.py
+++ github_access/test.py
@@ -37,10 +37,20 @@ def insert_repo(repo):
     contributors = repo.get_contributors()
     commits = repo.get_commits()
 
-    commitDict = {}
     for commit in commits:
-            print("inserting commit ",commit.commit.author.name, " ", commit.commit.author.date)
-            commitDict[commit.commit.author.name] = [commit.commit.author.name,commit.commit.author.date,[commit.stats.additions,commit.stats.deletions,commit.stats.total]]
+            #print("inserting commit ",commit.commit.author.name, " ", commit.commit.author.date)
+            author = commit.commit.author.name
+            datetime = commit.commit.author.date
+            additions = commit.stats.additions
+            deletions = commit.stats.deletions
+            total = commit.stats.total
+            db.commits.insert_one(
+            {
+             "repo" : repo.name,
+             "author": author,
+             "datatime": datetime,
+             "size": {"additions": additions, "deletions": deletions, "total": total}
+             })
     langList = repo.get_languages()
     contList = repo.get_contributors()
     contDict = {}
@@ -57,8 +67,7 @@ def insert_repo(repo):
          "name": repo.name,
          "description": repo.description,
          "language" : langDict,
-         "contributors": contDict,
-         "commits" : commitDict
+         "contributors": contDict
 
      })
 
@@ -68,7 +77,7 @@ def insert_repo(repo):
 client = pymongo.MongoClient("mongodb+srv://Anton:mongo@sweng-cqjlw.mongodb.net/test?retryWrites=true&w=majority")
 g2 = Github("4a78d9e6a5a0602050ccf60acf08cda73c530e96")
 #  4a78d9e6a5a0602050ccf60acf08cda73c530e96 github key
-user = g2.get_user("AntonYamkovoy")
+user = g2.get_user("yungene")
 
 
 #inserting collected data into db
