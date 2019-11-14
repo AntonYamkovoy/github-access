5f6d516376ecdf2621a57827c4093c7e8e8ea74d
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2019-11-14 08:18:55 +0000

    changes to api calls

--- github_access/test.py
+++ github_access/test.py
@@ -9,13 +9,24 @@ def insert_user(user):
     location = user.location
     nickname = user.name
     email = user.email
+    followerList = user.get_followers()
+    followingList = user.get_following()
+
+    followers = {}
+    following = {}
     if email is None:
         email = ""
     if nickname is None:
         nickname = ""
-    #followers_list = user.followers
-    #following_list = user.following
-    repositories = user.get_repos()
+    for f in followers:
+        followers[f.login] = f.login
+
+    for f in following:
+        following[f.login] = f.login
+
+
+
+
     # insert user information gathered into db
     print("inserting user ",user.login)
     db.users.insert_one(
@@ -23,10 +34,13 @@ def insert_user(user):
         "login": login,
         "name" : nickname,
         "location": location,
-        "email" : user.email
+        "email" : user.email,
+        "following" : following,
+        "followers" : followers
 
 
          })
+    repositories = user.get_repos()
     for repo in repositories:
         insert_repo(repo)
 
@@ -36,21 +50,11 @@ def insert_user(user):
 def insert_repo(repo):
     contributors = repo.get_contributors()
     commits = repo.get_commits()
+    commitDict = {}
 
     for commit in commits:
             #print("inserting commit ",commit.commit.author.name, " ", commit.commit.author.date)
-            author = commit.commit.author.name
-            datetime = commit.commit.author.date
-            additions = commit.stats.additions
-            deletions = commit.stats.deletions
-            total = commit.stats.total
-            db.commits.insert_one(
-            {
-             "repo" : repo.name,
-             "author": author,
-             "datatime": datetime,
-             "size": {"additions": additions, "deletions": deletions, "total": total}
-             })
+            commitDict[commit.sha] = (commit.commit.author.name, commit.commit.author.date, commit.stats.additions, commit.stats.deletions, commit.stats.total)
     langList = repo.get_languages()
     contList = repo.get_contributors()
     contDict = {}
@@ -67,7 +71,8 @@ def insert_repo(repo):
          "name": repo.name,
          "description": repo.description,
          "language" : langDict,
-         "contributors": contDict
+         "contributors": contDict,
+         "commits" : commitDict
 
      })
 
@@ -77,7 +82,7 @@ def insert_repo(repo):
 client = pymongo.MongoClient("mongodb+srv://Anton:mongo@sweng-cqjlw.mongodb.net/test?retryWrites=true&w=majority")
 g2 = Github("4a78d9e6a5a0602050ccf60acf08cda73c530e96")
 #  4a78d9e6a5a0602050ccf60acf08cda73c530e96 github key
-user = g2.get_user("yungene")
+user = g2.get_user("AntonYamkovoy")
 
 
 #inserting collected data into db
